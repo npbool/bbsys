0fcf7074e51393be7dcdc8df8fea5c3638735204
npbool <iwnking@gmail.com>
2016-07-08 01:42:51 +0800

    import/export init

--- crew/util.py
+++ crew/util.py
@@ -1,5 +1,8 @@
 from django.core import validators
-import csv
+from django.db import models
+from datetime import date, datetime
+import xlwt
+import xlrd
 
 
 class Choices:
@@ -37,11 +40,70 @@ class Choices:
 phone_validator = validators.RegexValidator(r"[\d|-]+", message="号码格式错误")
 
 
-def export_model(op, query_set, model_class):
-    writer = csv.writer(op)
+def to_representation(obj, field):
+    value = getattr(obj, field.name)
+    style = None
+    if isinstance(field, models.DateField):
+        style = xlwt.easyxf(num_format_str='YYYY-MM-DD')
+    if len(field.choices) > 0:
+        for ch in field.choices:
+            if ch[0] == value:
+                return ch[1], style
+        raise Exception(field.name + " " + str(value) + "Not found")
+    return value, style
+
+
+def to_value(rep, field, wb):
+    if len(field.choices) > 0:
+        for ch in field.choices:
+            if ch[1] == rep:
+                return ch[0]
+        raise Exception(field.name + " " + str(rep) + "Not found")
+    if isinstance(field, models.DateField):
+        year, month, day, *_ = xlrd.xldate_as_tuple(rep, wb.datemode)
+        return date(year, month, day)
+    return rep
+
+
+def export_model(query_set, model_class):
+    wb = xlwt.Workbook()
+    ws = wb.add_sheet("学生信息")
+
     fields = model_class._meta.fields
     columns = [field.verbose_name for field in fields]
-    writer.writerow(columns)
-    for obj in query_set:
-        values = [getattr(obj, field.name) for field in fields]
-        writer.writerow(values)
+    for j, c in enumerate(columns):
+        ws.write(0, j, c)
+    for i, obj in enumerate(query_set):
+        representations = [to_representation(obj, field) for field in fields]
+        for j, (rep, style) in enumerate(representations):
+            if style:
+                ws.write(i + 1, j, rep, style)
+            else:
+                ws.write(i + 1, j, rep)
+    wb.save("学生.xls")
+
+
+def import_model(filename, model_class):
+    fields = model_class._meta.fields
+    columns = [field.verbose_name for field in fields]
+    print(columns)
+
+    wb = xlrd.open_workbook(filename)
+    ws = wb.sheet_by_index(0)
+    header = ws.row_values(0)
+    print(header)
+
+    column_indices = [header.index(column) if column in header else None for column in columns]
+    not_found = [column for column, index in zip(columns, column_indices) if index is None]
+    if not_found:
+        raise Exception("缺少以下列: "+','.join(not_found))
+
+    kwargs = {}
+    for row_index in range(1, ws.nrows):
+        row = ws.row_values(row_index)
+        for field, index in zip(fields, column_indices):
+            kwargs[field.name] = to_value(row[index], field, wb)
+        print(kwargs)
+        s = model_class(**kwargs)
+        s.save()
+
